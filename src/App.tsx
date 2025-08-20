85d1254ec15fd43144e22b3e55c58bccd9010527
Tynahan <nusilite@gmail.com>
2025-08-20 13:53:06 +0900

    test commit to check cicd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>아무 이름이나 적어보기</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
